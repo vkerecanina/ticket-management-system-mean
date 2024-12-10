1d2cfcd8ff55e4d2eca50aa1245ef9b223fa33ee
Valentina <vkerechaninaa@gmail.com>
2024-12-10 18:27:44 +0000

    Set up environment

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+
+console.log(process.env.DATABASE_URL)  // check if DATABASE_URL is correct
+
+mongoose.connect(process.env.DATABASE_URL)
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to database'))
 
 app.listen(3000, () => console.log('Server Started'))
